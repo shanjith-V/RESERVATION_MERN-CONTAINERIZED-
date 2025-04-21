be2bd64cd04ecc414e6a6324fe183a0ebbd50851
shanjith-V <sanjumac0810@gmail.com>
2025-04-21 20:06:24 +0530

    Update Reservation.jsx

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -16,7 +16,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "http://15.206.209.83:4000/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
